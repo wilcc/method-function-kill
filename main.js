0f0fb7083ac752fd1ff75909d0cf375f59ccae03
Wilson Chen <wilson.chen@codeimmersives.com>
2020-04-07 16:07:30 -0400

    finish

--- main.js
+++ main.js
@@ -1,7 +1,40 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const newPerson = function(firstName,lastName,age,married=false){
+  const personinfo = {
+    firstName: firstName===undefined?'Anonymous':firstName,
+    lastName: lastName===undefined?'Person':lastName,
+    age,
+    married,
+    goingOn:function(){
+      return age + 1
+    },
+    ageUp:function(){
+      return this.age += 1
+    },
+    getFullName: function(){
+      return this.firstName + ' ' + this.lastName
+    },
+    marry: function(second){
+      this.married = !married
+      second.married = !married
+      this.spouseName = second.getFullName()
+      second.spouseName = this.getFullName()
 
+
+    },
+    divorce: function(second){
+      this.married = married
+      second.married = married
+      delete this.spouseName
+      delete second.spouseName
+    }
+    
+  }
+
+  return personinfo
+}
 
 
 /*********************************
